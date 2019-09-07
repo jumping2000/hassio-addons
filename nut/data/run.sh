6d4c35a4468924cd0d0a30a2595ca6d3e97b96d9
Gianpiero <gpammendola@yahoo.it>
2019-09-08 01:38:21 +0200

    Update run.sh

--- nut/data/run.sh
+++ nut/data/run.sh
@@ -16,7 +16,7 @@ if [ "$LOGINS" -gt "0" ]; then
         USERNAME=$(jq --raw-output ".logins[$i].username" $CONFIG_PATH)
         PASSWORD=$(jq --raw-output ".logins[$i].password" $CONFIG_PATH)
         INSTCMDS=$(jq --raw-output ".logins[$i].instcmds" $CONFIG_PATH)
-	echo "[$USERNAME]"            >> /etc/nut/upsd.users
+	    echo "[$USERNAME]"            >> /etc/nut/upsd.users
         echo "  password = $PASSWORD" >> /etc/nut/upsd.users
         echo "  instcmds = $INSTCMDS" >> /etc/nut/upsd.users
         echo ""                       >> /etc/nut/upsd.users
@@ -29,41 +29,46 @@ BATTERY=$(jq --raw-output ".battery | length" $CONFIG_PATH)
 RUNTIMECAL=$(jq --raw-output ".runtimecal | length" $CONFIG_PATH)
 UPS=$(jq --raw-output ".ups | length" $CONFIG_PATH)
 echo "" > /etc/nut/ups.conf
-#if [ "$POLLINTERVAL" -gt "0" ]; then
-#    echo "  pollinterval = $POLLINTERVAL" >> /etc/nut/ups.conf
-#fi
 if [ "$UPS" -gt "0" ]; then
     for (( i=0; i < "$LOGINS"; i++ )); do
         UPSNAME=$(jq --raw-output ".ups[$i].upsname" $CONFIG_PATH)
         DRIVER=$(jq --raw-output ".ups[$i].driver" $CONFIG_PATH)
         PORT=$(jq --raw-output ".ups[$i].port" $CONFIG_PATH)
+        PROTOCOL=$(jq --raw-output ".ups[$i].protocol" $CONFIG_PATH)
         VENDORID=$(jq --raw-output ".ups[$i].vendorid" $CONFIG_PATH)
         PRODUCTID=$(jq --raw-output ".ups[$i].productid" $CONFIG_PATH)
-	DESC=$(jq --raw-output ".ups[$i].desc" $CONFIG_PATH)
-	BATTVOLTHIGH=$(jq --raw-output ".battery[$i].battery_voltage_high" $CONFIG_PATH)
-	BATTVOLTLOW=$(jq --raw-output ".battery[$i].battery_voltage_low" $CONFIG_PATH)
-	CHARGETIME=$(jq --raw-output ".battery[$i].charge_time" $CONFIG_PATH)
-	RUNTIME1=$(jq --raw-output ".runtimecal[$i].runtime1" $CONFIG_PATH)
-	POWER1=$(jq --raw-output ".runtimecal[$i].power1" $CONFIG_PATH)
-	RUNTIME2=$(jq --raw-output ".runtimecal[$i].runtime2" $CONFIG_PATH)
-	POWER2=$(jq --raw-output ".runtimecal[$i].power2" $CONFIG_PATH)
+        DESC=$(jq --raw-output ".ups[$i].desc" $CONFIG_PATH)
+        BATTVOLTHIGH=$(jq --raw-output ".battery[$i].battery_voltage_high" $CONFIG_PATH)
+        BATTVOLTLOW=$(jq --raw-output ".battery[$i].battery_voltage_low" $CONFIG_PATH)
+        CHARGETIME=$(jq --raw-output ".battery[$i].charge_time" $CONFIG_PATH)
+        IDLELOAD=$(jq --raw-output ".battery[$i].idle_load" $CONFIG_PATH)
+        RUNTIME1=$(jq --raw-output ".runtimecal[$i].runtime1" $CONFIG_PATH)
+        POWER1=$(jq --raw-output ".runtimecal[$i].power1" $CONFIG_PATH)
+        RUNTIME2=$(jq --raw-output ".runtimecal[$i].runtime2" $CONFIG_PATH)
+        POWER2=$(jq --raw-output ".runtimecal[$i].power2" $CONFIG_PATH)
         echo "[$UPSNAME]"         >> /etc/nut/ups.conf
         echo "  driver = $DRIVER" >> /etc/nut/ups.conf
         echo "  port = $PORT"	  >> /etc/nut/ups.conf
-        if [ "$VENDORID" -gt "0" ]; then
+        if [ "$PROTOCOL" != "null" ]; then
+            echo "  protocol = $PROTOCOL"     >> /etc/nut/ups.conf
+        fi
+        if [ "$VENDORID" != "null" ]; then
             echo "  vendorid = $VENDORID"     >> /etc/nut/ups.conf
         fi
-        if [ "$PRODUCTID" -gt "0" ]; then
+        if [ "$PRODUCTID" != "null" ]; then
             echo "  productid = $PRODUCTID"   >> /etc/nut/ups.conf
         fi
-        if [ "$BATTVOLTHIGH" -gt "0" ]; then
+        if [ "$BATTVOLTHIGH" != "null" ]; then
             echo "  default.battery.voltage.high = $BATTVOLTHIGH"     >> /etc/nut/ups.conf
         fi
-        if [ "$BATTVOLTLOW" -gt "0" ]; then
+        if [ "$BATTVOLTLOW" != "null" ]; then
             echo "  default.battery.voltage.low = $BATTVOLTLOW"   >> /etc/nut/ups.conf
         fi        
-        if [ "$CHARGETIME" -gt "0" ]; then
+        if [ "$CHARGETIME" != "null" ]; then
             echo "  chargetime = $CHARGETIME"   >> /etc/nut/ups.conf
+        fi
+        if [ "$IDLELOAD" != "null" ]; then
+            echo "  idleload = $IDLELOAD"   >> /etc/nut/ups.conf
         fi 
         echo "  runtimecal = $RUNTIME1,$POWER1,$RUNTIME2,$POWER2" >> /etc/nut/ups.conf
         echo "  desc = $DESC"             >> /etc/nut/ups.conf
@@ -93,6 +98,7 @@ upsdrvctl start
 upsd -D
 
 WAIT_PIDS+=($!)
+
 # Register stop
 function stop_nut() {
     echo "[INFO] Kill Processes..."
